8cb39a88e21c755c415778a58c42a468b0f6071e
eger001 <mainforphp@gmail.com>
2023-01-25 23:33:39 +0200

    admin page improved form

--- app/views/pages/admin.php
+++ app/views/pages/admin.php
@@ -1,6 +1,7 @@
-<form>
+<form action="<?= url('admin', 'login')?>" method="post" >
     <label for="login">Login:</label>
-    <input type="text" name="login" value="administrator" readonly/>
+    <input type="text" name="login" autofocus/>
     <label for="pass">Password</label>
-    <input type="password" name="pass" autofocus/>
+    <input type="password" name="pass"/>
+    <input type="submit" value="Log In"/>
 </form>
