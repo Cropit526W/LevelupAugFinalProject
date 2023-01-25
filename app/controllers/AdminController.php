c4d11c56df66c215f105302cdee7e9b504901538
eger001 <mainforphp@gmail.com>
2023-01-25 23:32:35 +0200

    AdminController func login created \ must be improved!

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\controllers;
 use app\core\AbstractController;
+use app\core\Route;
+use app\models\UserModel;
 
 class AdminController extends AbstractController
 {
@@ -8,11 +10,23 @@ class AdminController extends AbstractController
     public function __construct()
     {
         parent::__construct('admin');
+        $this->model = new UserModel();
     }
 
 
     public function index()
     {
         $this->view->render('admin');
+    }
+
+    public function login()
+    {
+        $result = $this->model->get();
+        Route::redirect('admin', 'main');
+    }
+
+    public function main()
+    {
+        $this->view->render('admin_main');
     }
 }
