a1a1b55d4de1584708e3992dbdf80d4a92699fe9
eger001 <mainforphp@gmail.com>
2023-01-25 20:59:49 +0200

    admin page created

---
+++ app/views/pages/admin.php
@@ -0,0 +1,6 @@
+<form>
+    <label for="login">Login:</label>
+    <input type="text" name="login" value="administrator" readonly/>
+    <label for="pass">Password</label>
+    <input type="password" name="pass" autofocus/>
+</form>
