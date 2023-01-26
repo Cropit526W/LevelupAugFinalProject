a8ee99744d442eafb2a335248aad85afe4ada6d1
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-26 20:32:59 +0200

    view admin

--- app/views/pages/admin.php
+++ app/views/pages/admin.php
@@ -1,7 +1,7 @@
-<form action="<?= url('user', 'login')?>" method="post" >
+<form action="<?= url('admin', 'login')?>" method="post">
     <label for="login">Login:</label>
-    <input type="text" name="login" autofocus/>
+    <input type="text" name="login" id="login" autofocus/>
     <label for="pass">Password</label>
-    <input type="password" name="pass"/>
+    <input type="password" name="pass" id="pass"/>
     <input type="submit" value="Log In"/>
 </form>
