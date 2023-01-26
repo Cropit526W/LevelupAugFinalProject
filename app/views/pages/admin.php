72042b2062d18839cdb06b916d6a6f5b97940fdb
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-27 00:10:16 +0200

    view admin

--- app/views/pages/admin.php
+++ app/views/pages/admin.php
@@ -1,7 +1,11 @@
-<form action="<?= url('admin', 'login')?>" method="post">
-    <label for="login">Login:</label>
-    <input type="text" name="login" id="login" autofocus/>
-    <label for="pass">Password</label>
-    <input type="password" name="pass" id="pass"/>
-    <input type="submit" value="Log In"/>
+<form class="w3-container" action="<?= url('admin', 'login')?>" method="post">
+    <div class="w3-panel">
+        <label for="login">Login:</label>
+        <input class="w3-input w3-border w3-animate-input" type="text" name="login" id="login" autofocus/>
+        <label for="pass">Password</label>
+        <input class="w3-input w3-border w3-animate-input" type="password" name="pass" id="pass"/>
+    </div>
+    <div class="w3-panel">
+        <input class="w3-button w3-green w3-round" type="submit" value="Log In"/>
+    </div>
 </form>
