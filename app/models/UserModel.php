4ba980425224ceca687b02a017f33af9a47b5510
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-26 20:36:09 +0200

    UserModel

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -6,31 +6,79 @@ use app\core\Route;
 use mysqli;
 class UserModel extends Model
 {
-    public function get()
+    /**
+     * Let's get all the logins from the database
+     * @return array
+     */
+    public function get() : array
     {
         if (!empty($_POST['login']))
         {
             $stmt = $this->db->prepare("SELECT login FROM users");
             $stmt->execute();
             $result = $stmt->get_result();
-            $result = $result->fetch_assoc();
+
+            $users = [];
+            while ($user = $result->fetch_assoc())
+            {
+                $users[] = $user;
+            }
+
         }else {
             exit('oops!');
         }
-        return $result;
+        return $users;
     }
 
-    public function add(array $user)
+    public function isUser($user) : bool
     {
-        //TODO get func with elements from $_POST
-        //TODO I suggest do it with prepared queries
+        $users = $this->get();
+        return in_array($user['login'], array_column($users, 'login'));
     }
 
-    public function del()
+    /**
+     * Get all users
+     * @return array
+     */
+    public function all()
     {
-        //TODO del func and use $stmt->bind_param cause we have a variable in the query
-        //TODO $stmt = $this->db->prepare("DELETE FROM tasks WHERE id = ?");
-        //        $stmt->bind_param('i', $_POST['id']);
-        //        $stmt->execute();
+        $sql = "SELECT * FROM users;";
+        $result = $this->db->query($sql);
+        if (!$result) {
+            // TODO create log
+            exit('some problem with select user');
+        }
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    /**
+     * Add a new user to the database
+     * @param array $user
+     * @return void
+     */
+    public function add(array $user) : void
+    {
+        if (!$this->isUser($user)) {
+            $sql = "INSERT INTO users (login, pass) VALUES ('{$user['login']}', '{$user['pass']}');";
+            $result = $this->db->query($sql);
+            if (!$result) {
+                // TODO create log
+                exit('some problem with insert user');
+            }
+        } else {
+            // TODO have user;
+        }
+    }
+
+    /**
+     * Let's delete the user by id from the database
+     * @param $id
+     * @return void
+     */
+    public function delete($id) : void
+    {
+       $stmt = $this->db->prepare("DELETE FROM users WHERE id = ?");
+        $stmt->bind_param('i', $_POST['id']);
+        $stmt->execute();
     }
 }
