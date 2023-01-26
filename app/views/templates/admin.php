36a7137f09da8383165ddf6830ab544b43b94d61
eger001 <mainforphp@gmail.com>
2023-01-26 05:31:39 +0200

    admin template - included libraries and files

--- app/views/templates/admin.php
+++ app/views/templates/admin.php
@@ -6,6 +6,9 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+        <link rel="stylesheet" href="/css/admin_main.css">
     </head>
     <body>
         <header>
@@ -14,5 +17,6 @@
         <main>
             <?php include_once self::getPagePath() ?>
         </main>
+        <script src="/js/createNewUser.js"></script>
     </body>
 </html>
