b9e1c25fde514a2c60b4e78f20f4cb54d884af57
eger001 <mainforphp@gmail.com>
2023-01-26 05:30:35 +0200

    ads_create & user_create - added layouts for forms

--- app/views/pages/ads_create.php
+++ app/views/pages/ads_create.php
@@ -1,3 +1,10 @@
-<form action="" method="post">
-    <!-- ............. -->
+<h2>New bulletin</h2>
+<form action="" method="post" enctype="multipart/form-data">
+    <label></label>
+    <input type="text" name=""/>
+    <label></label>
+    <textarea name=""></textarea>
+    <label></label>
+    <input type="file" name="" accept=""/>
+    <input type="submit"/>
 </form>
--- app/views/pages/user_create.php
+++ app/views/pages/user_create.php
@@ -1,3 +1,17 @@
 <h2>Here we're creating new users</h2>
 <h3>Here will be a form, where new users must be creating</h3>
 <h3>And here will be a table with all users, where must be username, and action 'delete user'</h3>
+
+<li class="form">Click to create new user<i class="fa fa-angle-down pull-right"></i></li>
+<form action="" method="post" class="dropdown-form">
+    <ul>
+        <label for="login">Login</label>
+        <li><input type="text" name="login"/></li>
+        <label for="pass">Password</label>
+        <li><input type="password" name="pass"/></li>
+        <li><input type="submit" value="Create"/></li>
+    </ul>
+</form>
+
+
+
