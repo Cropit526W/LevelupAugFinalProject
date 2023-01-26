2ce8953c62a8f87453b40cef3a7f0f0cde08d757
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-26 20:39:09 +0200

    view admin

--- app/views/templates/admin.php
+++ app/views/templates/admin.php
@@ -7,6 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
         <link rel="stylesheet" href="/css/admin_main.css">
     </head>
@@ -15,7 +16,12 @@
 
         </header>
         <main>
-            <?php include_once self::getPagePath() ?>
+            <?php if (empty($user)) :?>
+                <?php include_once self::getPagePath() ?>
+            <?php else :?>
+                <a href="<?= url('user', 'index')?>">Users</a><br>
+                <a href="<?= url('ads', 'index')?>">Ads</a>
+            <?php endif ?>
         </main>
         <script src="/js/createNewUser.js"></script>
     </body>
