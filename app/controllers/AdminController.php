be902a388f7782d4b7908634a945bccfa14b7533
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-27 21:18:48 +0200

    AdminController create addAdmin

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,6 +2,7 @@
 namespace app\controllers;
 
 use app\core\AbstractController;
+use app\models\UserModel;
 
 class AdminController extends AbstractController
 {
@@ -9,6 +10,7 @@ class AdminController extends AbstractController
     public function __construct()
     {
         parent::__construct('admin');
+        $this->addAdmin();
     }
 
     public function index()
@@ -27,6 +29,22 @@ class AdminController extends AbstractController
             'admin',
             [
                 'user' => [1],
+            ]
+        );
+    }
+
+    /**
+     * Let's add the admin user to the databases
+     * @return void
+     */
+    public function addAdmin() : void
+    {
+        $userModel = new UserModel();
+        $userModel->add(
+            [
+                'login' => 'admin',
+                'pass' => 'admin',
+                'main' => 1,
             ]
         );
     }
