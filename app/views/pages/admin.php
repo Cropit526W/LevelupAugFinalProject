9f9f104f0b51a20060f5554503ed1e083c3bf0db
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-27 22:28:29 +0200

    view admin

--- app/views/pages/admin.php
+++ app/views/pages/admin.php
@@ -1,9 +1,9 @@
 <form class="w3-container" action="<?= url('admin', 'login')?>" method="post">
     <div class="w3-panel">
         <label for="login">Login:</label>
-        <input class="w3-input w3-border w3-animate-input" type="text" name="login" id="login" autofocus/>
+        <input class="w3-input w3-border w3-animate-input" type="text" name="login" id="login" required autofocus/>
         <label for="pass">Password</label>
-        <input class="w3-input w3-border w3-animate-input" type="password" name="pass" id="pass"/>
+        <input class="w3-input w3-border w3-animate-input" type="password" name="pass" id="pass" required/>
     </div>
     <div class="w3-panel">
         <input class="w3-button w3-green w3-round" type="submit" value="Log In"/>
