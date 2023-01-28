429396a5653eb209051f972a85a4a088bf6b2423
Serkhio <pardinaserkhio@gmail.com>
2023-01-28 20:26:31 +0200

    ads form

--- app/views/pages/ads_create.php
+++ app/views/pages/ads_create.php
@@ -1,11 +1,11 @@
 <h2>New bulletin</h2>
 <form action="" method="post" enctype="multipart/form-data">
-    <label></label>
-    <input type="text" name=""/>
-    <label></label>
-    <textarea name=""></textarea>
-    <label></label>
-    <input type="file" name="" accept=""/>
+    <label for="headline">Title of bulletin</label>
+    <input type="text" name="headline" id="headline"/>
+    <label for="description">Description</label>
+    <textarea name="description" id="description"></textarea>
+    <label>Photos</label>
+    <input type="file" name="photo" accept="image/*" multiple/>
     <input type="submit"/>
 </form>
 <a href="<?= url('ads', 'index')?>">Back</a>
