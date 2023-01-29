49f00bc32f4da7e6824f35c79f65fbca85d082ed
Cropit526W <117841702+Cropit526W@users.noreply.github.com>
2023-01-29 13:34:41 +0200

    added mysqli error by exception error code

--- app/core/Model.php
+++ app/core/Model.php
@@ -15,10 +15,14 @@ class Model
     {
         try{
             $this->db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        } catch (mysqli_sql_exception){
-            CreateDB::create();
-            AdminController::addAdmin();
-            $this->db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        } catch (mysqli_sql_exception $exception){
+            if($exception->getCode() === 1049){
+                CreateDB::create();
+                AdminController::addAdmin();
+                $this->db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            } else {
+                exit('Some problem with connection to database');
+            }
         }
     }
 }
