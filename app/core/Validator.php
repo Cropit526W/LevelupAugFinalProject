a845f525de28039609f90b54370e3dc78037c2bb
vladislav <vladislav.nikitin.home@gmail.com>
2023-02-02 15:40:16 +0200

    Validator.php change

--- app/core/Validator.php
+++ app/core/Validator.php
@@ -8,9 +8,13 @@ class Validator
 {
     protected array $errors = [];
 
+    protected $session;
+
     public function __construct()
     {
         $this->model = new UserModel();
+        $this->session = new Session();
+
     }
 
     public function validateName($name)
@@ -37,11 +41,27 @@ class Validator
      * @param $user
      * @return array
      */
-    public function userErrors($user) : array
+    public function validateAddUser($user) : array
     {
-        if ($this->model->is($user)) {
+        if (!empty($this->model->get($user['login']))) {
             $this->errors[] = 'This user already exists in the database';
         }
+
+        if (count($this->errors) > 0) {
+            $this->session->save('Add new user', $this->errors);
+        }
+
         return $this->errors;
+    }
+
+    /**
+     * Returns a list of errors about delete the user
+     * @param string $msg
+     * @return void
+     */
+    public function validateDeleteUser(string $msg) : void
+    {
+        $this->errors[] = $msg;
+        $this->session->save('Delete user', $this->errors);
     }
 }
