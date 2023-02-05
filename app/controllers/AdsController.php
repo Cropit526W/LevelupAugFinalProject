9b4080dad7ead446686f3da6a399d0fd4929472d
eger001 <mainforphp@gmail.com>
2023-02-05 22:29:17 +0200

    modified create() with validation

--- app/controllers/AdsController.php
+++ app/controllers/AdsController.php
@@ -33,55 +33,54 @@ class AdsController extends AdminController
         $this->view->render('ads_create');
     }
 
+
     public function store()
     {
-        $file = $_FILES['photos'];
-        $fileTest = [];
-        $file_count = count($file['name']);
-        $file_keys = array_keys($file);
-        for ($i = 0; $i < $file_count; $i++) {
-            foreach ($file_keys as $key) {
-                $fileTest[$i][$key] = $file[$key][$i];
+        if (!empty($_FILES['photos'])) {
+            $file = $_FILES['photos'];
+            $fileTest = [];
+            $file_count = count($file['name']);
+            $file_keys = array_keys($file);
+            for ($i = 0; $i < $file_count; $i++) {
+                foreach ($file_keys as $key) {
+                    $fileTest[$i][$key] = $file[$key][$i];
+                }
             }
+            //TODO validate
+            if ($_SERVER['HTTP_REFERER'] !== 'http://levelupaugfinalproject1/ads/create') {
+                $adsPhotoErrors = $this->validate->fileValidate($file);
+                if (count($adsPhotoErrors) !== 0) {
+                    //TODO SESSION
+                    $this->view->render(
+                        'ads_create',
+                        [
+                            'adsPhotoErrors' => $adsPhotoErrors,
+                        ]
+                    );
+                    Route::redirect('ads', 'create');
+                }
+                $vendorCode = current($_POST);
+                $ad_id = array_key_first($_POST);
+                $this->model->updatedPhotoDirAdd($fileTest, $vendorCode);
+                Route::redirect('ads', 'index#'.$ad_id);
+            } else {
+                //TODO array
+                $headline = filter_input(INPUT_POST, 'headline');
+                $description = filter_input(INPUT_POST, 'description');
+                $author = filter_input(INPUT_POST, 'author');
+                $phone = filter_input(INPUT_POST, 'phone');
+                $adsTextErrors = $this->validate->textValidator($headline, $description, $author, $phone);
+                $adsPhotoErrors = $this->validate->fileValidate($file);
+                if (count($adsTextErrors) > 0 || count($adsPhotoErrors) > 0) {
+                    $this->validate->setErrors($adsTextErrors, $adsPhotoErrors);
+                    Route::redirect('ads', 'create');
+                }
+            }
+            $this->model->photoDirAdd($fileTest, $headline, $description, $author, $phone);
+            Route::redirect('index', 'index');
         }
-        if ($_SERVER['HTTP_REFERER'] !== 'http://levelupaugfinalproject1/ads/create') {
-            $adsPhotoErrors = $this->validate->fileValidate($file);
-            if (count($adsPhotoErrors) !== 0) {
-                $this->view->render(
-                    'ads_create',
-                    [
-                        'adsPhotoErrors' => $adsPhotoErrors,
-                    ]
-                );
-                Route::redirect('ads', 'create');
-            }
-            $vendorCode = current($_POST);
-            $ad_id = array_key_first($_POST);
-            $this->model->updatedPhotoDirAdd($fileTest, $vendorCode);
-            Route::redirect('ads', 'index#'.$ad_id);
-        } else {
-            $headline = filter_input(INPUT_POST, 'headline');
-            $description = filter_input(INPUT_POST, 'description');
-            $author = filter_input(INPUT_POST, 'author');
-            $phone = filter_input(INPUT_POST, 'phone');
-            $adsTextErrors = $this->validate->textValidator($headline, $description, $author, $phone);
-            $adsPhotoErrors = $this->validate->fileValidate($file);
-            if (count($adsTextErrors) !== 0 || count($adsPhotoErrors) !== 0) {
-                $this->view->render(
-                    'ads_create',
-                    [
-                        'adsTextErrors' => $adsTextErrors,
-                        'adsPhotoErrors' => $adsPhotoErrors,
-                    ]
-                );
-                Route::redirect('ads', 'create');
-            }
-        }
-        $this->model->photoDirAdd($fileTest);
-        $this->model->textDbAdd($headline, $description, $author, $phone);
-        Route::redirect('index', 'index');
-
     }
+
 
     /**
      * delete selected ad
@@ -90,6 +89,7 @@ class AdsController extends AdminController
     public function destroy()
     {
         if (empty($_POST['id'])) {
+            // TODO validate
             $url = current($_POST);
             $ad_id = array_key_first($_POST);
             $this->model->delPhotoFromAd($url);
@@ -99,6 +99,7 @@ class AdsController extends AdminController
         $this->model->del();
         Route::redirect('ads', 'index');
     }
+
 
     public function edit()
     {
